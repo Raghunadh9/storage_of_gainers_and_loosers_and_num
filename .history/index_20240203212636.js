d1c929b765624dc3a926ee9a8af82bbaa433737a
Raghunadh <konidoniravirr@gmail.com>
2024-02-03 21:47:27 +0530

    first_commit

---
+++ .history/index_20240203212636.js
@@ -0,0 +1,26 @@
+import express from "express";
+import cors from "cors";
+const app = express();
+app.use(cors());
+app.get("/", async (req, res) => {
+  try {
+    // Read the contents of the data.json file
+    const fileContents = fs.readFileSync("./data.json", "utf-8");
+    const jsonData = JSON.parse(fileContents);
+    res.status(200).json(jsonData);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: error });
+  }
+});
+
+app.get("/num_data", async (req, res) => {
+  try {
+    const fileContents_num = fs.readFileSync("./data_num.json", "utf-8");
+    const jsonData_num = JSON.parse(fileContents_num);
+    res.status(200).json(jsonData_num);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: error });
+  }
+});
